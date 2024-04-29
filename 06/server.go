1dde4b5957e2de92d651ee0ee9e9c2e451c182c5
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2024-04-29 12:04:50 +0900

    chapter6: pipelining

--- 06/server.go
+++ 06/server.go
@@ -8,49 +8,70 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"strings"
+	"time"
 )
 
-// 青空文庫: ごんぎつねより
-// https://www.aozora.gr.jp/cards/000121/card628.html
-var contents = []string{
-	" これは、私わたしが小さいときに、村の茂平もへいというおじいさんからきいたお話です。",
-	" むかしは、私たちの村のちかくの、中山なかやまというところに小さなお城があって、",
-	" 中山さまというおとのさまが、おられたそうです。",
-	" その中山から、少しはなれた山の中に、「ごん狐ぎつね」という狐がいました。",
-	" ごんは、一人ひとりぼっちの小狐で、しだの一ぱいしげった森の中に穴をほって住んでいました。",
-	" そして、夜でも昼でも、あたりの村へ出てきて、いたずらばかりしました。",
+// 順番に従って conn に書き出しをする (goroutine で実行される)
+func writeToConn(sessionResponses chan chan *http.Response, conn net.Conn) {
+	defer conn.Close()
+	// 順番に取り出す
+	for sessionResponse := range sessionResponses {
+		// 選択された仕事が終わるまで待つ
+		response := <-sessionResponse
+		response.Write(conn)
+		close(sessionResponse)
+	}
+}
+
+// セッション内のリクエストを処理する
+func handleRequest(request *http.Request, resultReceiver chan *http.Response) {
+	dump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(dump))
+	content := "Hello World\n"
+	// レスポンスを書き込む
+	// セッションを維持するためにKeep-Aliveでないといけない
+	response := &http.Response{
+		StatusCode:    200,
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		ContentLength: int64(len(content)),
+		Body:          io.NopCloser(strings.NewReader(content)),
+	}
+	// 処理が終わったらチャネルに書き込み、
+	// ブロックされていたwriteToConnの処理を再始動する
+	resultReceiver <- response
 }
 
 func processSession(conn net.Conn) {
 	fmt.Printf("Accept %v\n", conn.RemoteAddr())
-	defer conn.Close()
+	// セッション内のリクエストを順に処理するためのチャネル
+	sessionResponses := make(chan chan *http.Response, 50)
+	defer close(sessionResponses)
+	// レスポンスを直列化してソケットに書き出す専用のgoroutine
+	go writeToConn(sessionResponses, conn)
+	reader := bufio.NewReader(conn)
 	for {
+		// レスポンスを受け取ってセッションのキューに入れる
+		conn.SetReadDeadline(time.Now().Add(5 * time.Second))
 		// リクエストを読み込む
-		request, err := http.ReadRequest(bufio.NewReader(conn))
+		request, err := http.ReadRequest(reader)
 		if err != nil {
-			if err == io.EOF {
+			neterr, ok := err.(net.Error)
+			if ok && neterr.Timeout() {
+				fmt.Println("Timeout")
+				break
+			} else if err == io.EOF {
 				break
 			}
 			panic(err)
 		}
-		dump, err := httputil.DumpRequest(request, true)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(string(dump))
-
-		// レスポンスを書き込む
-		fmt.Fprintf(conn, strings.Join([]string{
-			"HTTP/1.1 200 OK",
-			"Content-Type: text/plain",
-			"Transfer-Encoding: chunked",
-			"", "",
-		}, "\r\n"))
-		for _, content := range contents {
-			bytes := []byte(content)
-			fmt.Fprintf(conn, "%x\r\n%s\r\n", len(bytes), content)
-		}
-		fmt.Fprintf(conn, "0\r\n\r\n")
+		sessionResponse := make(chan *http.Response)
+		sessionResponses <- sessionResponse
+		// 非同期でレスポンスを実行
+		go handleRequest(request, sessionResponse)
 	}
 }
 
